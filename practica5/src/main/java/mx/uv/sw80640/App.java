fbaf0e2b027182540031aec997fb6da1e1917ed2
RafaelUV18 <rafaelvazquez889@gmail.com>
2020-10-21 13:04:44 -0500

    Se agrega practica 7

--- practica5/src/main/java/mx/uv/sw80640/App.java
+++ practica5/src/main/java/mx/uv/sw80640/App.java
@@ -1,5 +1,10 @@
 package mx.uv.sw80640;
+
 import static spark.Spark.*;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 /**
  * Hello world!
  *
@@ -9,6 +14,49 @@ public class App
     public static void main( String[] args )
     {
         port(2020);
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+
+        
         get("/", (req,res)->"Hola desde Spark");
+        get("/hola",(req,res)->{
+            //System.out.println(""+req.body());
+            //System.out.println("Request"+req.contentType());
+            System.out.println("Request: "+req.queryParams());
+            return"Hola " + req.queryParams("PrmEmail")+" desde Spark";
+        });
+
+        post("/adios",(req,res)->{
+            //System.out.println(""+req.body());
+            System.out.println("Request"+req.contentType());
+            System.out.println("Request: "+req.queryParams());
+            System.out.println("Request"+req.contentType());
+            return"Adios " + req.queryParams("PrmEmail")+" desde Spark";
+        });
+
+        post("/adiosJson",(req,res)->{
+            JsonParser parser=new JsonParser();
+            JsonElement arbol=parser.parse(req.body());
+            JsonObject peticion=arbol.getAsJsonObject();
+
+            System.out.println("Request"+req.contentType());
+            System.out.println("Request: "+req.queryParams());
+            System.out.println("Request"+req.contentType());
+            return"Adios " + req.queryParams("PrmEmail")+" desde Spark";
+        });
     }
 }
